e89cc41ab5d4bae7367891164772ae23c6f48631
GJ Viviers <hardusviviers@gmail.com>
2024-01-29 17:13:04 +0100

    reworked gamemanager and particle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,116 +4,48 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Prefabs")]
+    [Header("---Prefabs---")]
     [SerializeField] private GameObject _particlePrefab;
 
-    [Header("Settings")]
-    [SerializeField] private float _particleRadius = 0.3f;
-    [SerializeField] private float _particleInfluenceRadius = 1f;
-    [SerializeField] private float _particleFriction = 0.1f;
-    [SerializeField] private int _particleAmount = 100;
-    [SerializeField] private float _forceMultiplier = 5f;
-
-    [Header("Spawn Area")]
-    [SerializeField] private Vector2 _spawnAreaRatio = new Vector2(10, 10); // {Height, Width}
-    [SerializeField] private Vector2 _spawnAreaCenter = new Vector2(0, 0); // {x, y}
-    private Vector4 _bounds; // {minX, maxX, minY, maxY}
-
-    [Header("Colors")]
+    [Header("---Color Palette---")]
     [SerializeField] private Color[] _colorPalette = new Color[] { Color.red };
-    private float[][] _colorWeights;
+
+    [Header("---Particle Settings---")]
+    [SerializeField] private float _radius; // static
+    [SerializeField] private float _maxInfluenceRadius; // static
+    [SerializeField] private float _maxDetectionRadius; // static 
+    [SerializeField] private int _particleCount = 20;
 
     private List<GameObject> _particles = new List<GameObject>();
 
-    // Gizmos
-    private void OnDrawGizmos()
-    {
-        // Draw spawn area
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(_spawnAreaCenter, _spawnAreaRatio);
-    }
-
-    // Start is called before the first frame update
     private void Start()
     {
-        SetupSettings();
-        SpawnParticles(_particleAmount);
+        SetStaticVariables();
+        SpawnParticles(_particleCount);
     }
 
-    // Update is called once per frame
-    private void Update()
+    private void SetStaticVariables()
     {
-        foreach (GameObject particle in _particles)
-        {
-            foreach (GameObject otherParticle in _particles)
-            {
-                if (particle != otherParticle)
-                {
-                    Particle.ApplyForces(particle, otherParticle);
-                }
-            }
-        }
-    }
-
-    private void PopulateRelationMatrix()
-    {
-        _colorWeights = new float[_colorPalette.Length][];
-        for (int i = 0; i < _colorPalette.Length; i++)
-        {
-            _colorWeights[i] = new float[_colorPalette.Length];
-            for (int j = 0; j < _colorPalette.Length; j++)
-            {
-                if (i == j)
-                {
-                    _colorWeights[i][j] = 1;
-                }
-                else
-                {
-                    _colorWeights[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    private void SetupSettings()
-    {
-        // Calculate spawn area
-        float _minX = _spawnAreaCenter.x - (_spawnAreaRatio.x / 2);
-        float _maxX = _spawnAreaCenter.x + (_spawnAreaRatio.x / 2);
-        float _minY = _spawnAreaCenter.y - (_spawnAreaRatio.y / 2);
-        float _maxY = _spawnAreaCenter.y + (_spawnAreaRatio.y / 2);
-        _bounds = new Vector4(_minX, _maxX, _minY, _maxY);
-
-        // Set static variables
-        Particle._radius = _particleRadius;
-        Particle._influenceRadius = _particleInfluenceRadius;
-        Particle._colorPallet = _colorPalette;
-        Particle._bounds = _bounds;
-        Particle._friction = _particleFriction;
-        Particle._forceMultiplier = _forceMultiplier;
-
-        // Set color weights
-        PopulateRelationMatrix();
-        Particle._colorWeights = _colorWeights;
+        Particle._radius = _radius;
+        Particle._maxInfluenceRadius = _maxInfluenceRadius;
+        Particle._maxDetectionRadius = _maxDetectionRadius;
     }
 
     private void SpawnParticles(int amount)
     {
         for (int i = 0; i < amount; i++)
         {
-            Vector3 randomPos = new Vector3(Random.Range(_bounds.x, _bounds.y), Random.Range(_bounds.z, _bounds.w), 0);
-            Color randomColor = _colorPalette[Random.Range(0, _colorPalette.Length)];
-            SpawnParticle(randomColor, randomPos);
+            SpawnParticle(_colorPalette[Random.Range(0, _colorPalette.Length)]);
         }
-
     }
 
-    private void SpawnParticle(Color color, Vector3 position)
+    private void SpawnParticle(Color color)
     {
-        GameObject particleObject = Instantiate(_particlePrefab, position, Quaternion.identity);
-        particleObject.GetComponent<Particle>().InitParticle(color);
+        Vector3 randomPos = new Vector3(Random.Range(SpawnArea._bounds.x, SpawnArea._bounds.y), Random.Range(SpawnArea._bounds.z, SpawnArea._bounds.w), 0f);
+        GameObject particle = Instantiate(_particlePrefab, randomPos, Quaternion.identity);
+        particle.GetComponent<Particle>().Init(color);
 
         // Add particle to list
-        _particles.Add(particleObject);
+        _particles.Add(particle);
     }
 }
--- Assets/Scripts/Particle.cs
+++ Assets/Scripts/Particle.cs
@@ -6,127 +6,20 @@ public class Particle : MonoBehaviour
 {
     // Static variables
     public static float _radius;
-    public static float _influenceRadius;
-    public static Color[] _colorPallet;
-    public static Vector4 _bounds; // {minX, maxX, minY, maxY}
-    public static float _friction;
-    public static float _forceMultiplier;
-    public static float[][] _colorWeights;
+    public static float _maxInfluenceRadius;
+    public static float _maxDetectionRadius;
 
-    // Instance variables
-    public Color _color;
-    public Vector3 _velocity;
-    public System.Func<float, float>[] _forceFunctions;
+    // Local variables
+    private Color _color;
+    private Vector2 _velocity;
 
-    // PRIVATE METHODS
-    private void OnDrawGizmos()
+    // Components
+    private SpriteRenderer _sr;
+
+    public void Init(Color color)
     {
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(transform.position, _radius);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, _influenceRadius);
-    }
-
-    private void Update()
-    {
-        Move();
-    }
-
-    private void Move()
-    {
-        if (transform.position.x < _bounds.x || transform.position.x > _bounds.y)
-        {
-            _velocity.x *= -1;
-        }
-        if (transform.position.y < _bounds.z || transform.position.y > _bounds.w)
-        {
-            _velocity.y *= -1;
-        }
-
-        _velocity *= 1 - _friction;
-        transform.position += _velocity * Time.deltaTime;
-
-    }
-
-    // PUBLIC METHODS
-    public void InitParticle(Color color)
-    {
-        // Set variables
+        _sr = GetComponent<SpriteRenderer>();
         _color = color;
-
-        // Set sprite color
-        GetComponent<SpriteRenderer>().color = color;
-
-        // Set Scale to fit radius
-        transform.localScale = Vector3.one * _radius * 2;
-
-        // Set function
-        _forceFunctions = new System.Func<float, float>[_colorPallet.Length];
-        int colorIndex = GetColorIndex(color);
-
-        // Define piecewise functions for each color
-        for (int i = 0; i < _colorPallet.Length; i++)
-        {
-            float yControlPoint = _colorWeights[colorIndex][i];
-
-            _forceFunctions[i] = t =>
-            {
-                float y;
-                float x_radius = 0.3f;
-                float x_influence = 0.65f;
-
-                if (t < x_radius)
-                {
-                    // Line from (0,-1) to (0.3,0)
-                    y = Mathf.Lerp(-1, 0, t / x_radius);
-                }
-                else if (t < x_influence)
-                {
-                    // Line from (0.3,0) to (0.65,yControlPoint)
-                    y = Mathf.Lerp(0, yControlPoint, (t - x_radius) / (x_influence - x_radius));
-                }
-                else
-                {
-                    // Line from (0.65,yControlPoint) to (1,0)
-                    y = Mathf.Lerp(yControlPoint, 0, (t - x_influence) / (1 - x_influence));
-                }
-                return y;
-            };
-        }
-    }
-
-    // PUBLIC STATIC METHODS
-    public static void ApplyForces(GameObject receiver, GameObject applier)
-    {
-        Particle receiverParticle = receiver.GetComponent<Particle>();
-        Particle applierParticle = applier.GetComponent<Particle>();
-
-        // Calculate distance
-        float distance = Vector3.Distance(receiver.transform.position, applier.transform.position);
-
-        if (distance < _influenceRadius)
-        {
-            // Calculate direction
-            Vector3 direction = (applier.transform.position - receiver.transform.position).normalized;
-
-            // Calculate force
-            int receiverColorIndex = GetColorIndex(receiverParticle._color);
-            float force = applierParticle._forceFunctions[receiverColorIndex](distance / _influenceRadius) * _forceMultiplier;
-
-            // Apply force
-            receiverParticle._velocity += direction * force * Time.deltaTime;
-        }
-    }
-
-    public static int GetColorIndex(Color color)
-    {
-        for (int i = 0; i < _colorPallet.Length; i++)
-        {
-            if (_colorPallet[i] == color)
-            {
-                return i;
-            }
-        }
-        return -1;
+        _sr.color = color;
     }
 }
